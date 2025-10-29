27e57fc67e8812ebb538a6df553de6c5a5d8f5ac
alexandra <a.diiorio753@gmail.com>
2025-10-29 16:44:20 -0400

    question #5
    wotre 36 before I ment #6

--- Game.java
+++ Game.java
@@ -81,21 +81,9 @@ public class Game
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if(currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
-        System.out.println();
+        
+        PrintLocationInfo();
+        
     }
 
     /**
@@ -176,21 +164,7 @@ public class Game
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if(currentRoom.northExit != null) {
-                System.out.print("north ");
-            }
-            if(currentRoom.eastExit != null) {
-                System.out.print("east ");
-            }
-            if(currentRoom.southExit != null) {
-                System.out.print("south ");
-            }
-            if(currentRoom.westExit != null) {
-                System.out.print("west ");
-            }
-            System.out.println();
+            printLocationInfo();
         }
     }
 
@@ -210,4 +184,24 @@ public class Game
             return true;  
         }
     }
+    private void PrintLocationInfo(){
+        System.out.println("You are " + currentRoom.getDescription());
+        System.out.print("Exits: ");
+        if(currentRoom.northExit != null) {
+                System.out.print("north ");
+        }
+        if(currentRoom.eastExit != null) {
+                System.out.print("east ");
+        }
+        if(currentRoom.southExit != null) {
+                System.out.print("south ");
+        }
+        if(currentRoom.westExit != null) {
+                System.out.print("west ");
+        }
+        System.out.println();
+        }
+    }
+    
+
 }
